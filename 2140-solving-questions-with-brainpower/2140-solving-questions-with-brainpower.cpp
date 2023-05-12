65a2ab52ed2b14b90ad1eca0db7d865e28ead56b
Praveen-Dahiya <59509985+Praveen-Dahiya@users.noreply.github.com>
2023-05-12 14:16:28 +0530

    Time: 418 ms (11.69%), Space: 120.5 MB (64.05%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    long long dp[100001];
-    long long f(vector<vector<int>>& questions,int idx){
-        if(idx>=questions.size()){
-            return 0;
-        }
+   
+//     long long f(vector<vector<int>>& questions,int idx){
+//         if(idx>=questions.size()){
+//             return 0;
+//         }
         
-        if(dp[idx] !=-1){
-            return dp[idx];
-        }
+//         if(dp[idx] !=-1){
+//             return dp[idx];
+//         }
         
-         long long f1=questions[idx][0]+f(questions,idx+questions[idx][1]+1);
-         long long f2=f(questions,idx+1);
-        return dp[idx]=max(f1,f2);
-    }
+//          long long f1=questions[idx][0]+f(questions,idx+questions[idx][1]+1);
+//          long long f2=f(questions,idx+1);
+//         return dp[idx]=max(f1,f2);
+//     }
     long long mostPoints(vector<vector<int>>& questions) {
-        memset(dp,-1,sizeof(dp));
-        return f(questions,0);
+         long long dp[1000000]={0};
+        long long ans=0;
+        for(int i=questions.size()-1;i>=0;i--){
+            dp[i]=max(questions[i][0]+dp[questions[i][1] + i + 1],dp[i+1]);
+            
+        }
+        cout<<dp[0];
+        
+        return dp[0];
     }
 };
