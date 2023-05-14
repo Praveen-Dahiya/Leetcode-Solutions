f6a7a738c20421897629d65a2c4efa8ce48dea27
Praveen-Dahiya <59509985+Praveen-Dahiya@users.noreply.github.com>
2023-05-14 11:36:51 +0530

    Time: 78 ms (88.58%), Space: 9 MB (44.98%) - LeetHub

---
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int solve(vector<int>&nums, int mask, int countOfOp, vector<int>&dp, vector<vector<int>>& gcd) {
+        int m = nums.size(), n = nums.size()/2;
+        if (countOfOp == n) {
+            return 0;
+        }
+        if (dp[mask] != -1) {
+            return dp[mask];
+        }
+        int maxScore = 0;
+
+        for (int i = 0; i < m; ++i) {
+            if (((mask >> i) & 1) == 1) {
+                continue;
+            }
+            for (int j = i + 1; j < m; ++j) {
+    
+                if (((mask >> j) & 1) == 1) {
+                    continue;
+                }
+
+                int newMask = mask | (1 << i) | ((1 << j));
+
+
+                int pans = (countOfOp + 1) * gcd[i][j] +solve(nums, newMask, countOfOp + 1, dp, gcd);
+                
+
+                maxScore = max(maxScore, pans);
+
+            }
+        }
+        return dp[mask] = maxScore;
+    }
+
+    int maxScore(vector<int>& nums) {
+        int m = nums.size();
+        
+        int n = 1 << m; 
+        vector<int>dp(n, -1);
+
+        vector<vector<int>> gcd(m, vector<int>(m, 0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<m;j++){
+                gcd[i][j] = __gcd(nums[i], nums[j]);
+            }
+        }
+
+        return solve(nums, 0, 0, dp, gcd);
+    }
+};
